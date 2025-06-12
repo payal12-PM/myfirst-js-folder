1213f24a21fecc8d9be80a9e10e3a589359aa71f
payal12-PM <payalku0305@gmail.com>
2025-06-12 05:00:44 +0000

    datatypes

---
+++ 01_basic/datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as never version
+
+// alert(3+3)  we are using node js ,not browser
+//code readability that should be heigh
+console.log(3+4);
+console.log("Payal")
+
+
+
+//number=>2 to 53
+//bigint=>
+//String=>""
+//boolean=>true/false
+//null=>standalone value
+//unndefined=> let state;
+//symbole=>  uniqueness 
+
+//object
+
+
+let name ="Hitesh"
+let age=18
+let isLoggedIn=false
+
+console.log(typeof undefined);
+console.log(typeof null);
