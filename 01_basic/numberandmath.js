48f1f87134a308d62bcae0c4caea96de822794d5
payal12-PM <payalku0305@gmail.com>
2025-06-13 05:08:22 +0000

    numberandmath

---
+++ 01_basic/numberandmath.js
@@ -0,0 +1,37 @@
+// const score= 780;
+// console.log(score);
+
+// // const balance=new Number(100);
+// // console.log(balance);
+
+// // console.log(balance.toString().length);
+// // console.log(balance.toFixed(1));
+
+// // const otherNumber=123.8966;
+// // console.log(otherNumber.toPrecision(4));
+
+
+// const hundred=1000000;
+// console.log(hundred.toLocaleString('en-IN'));
+
+// //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+//absolute
+console.log(Math.abs(-4));
+//around 
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.3));
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min =10;
+const max =20;
+
+console.log(Math.random()*(max-min+1));
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
