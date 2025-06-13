e44027aedd5e90fac9bccc03a70d8b7b285f3cc0
payal12-PM <payalku0305@gmail.com>
2025-06-13 04:27:40 +0000

    string

--- 01_basic/String.js
+++ 01_basic/String.js
@@ -5,7 +5,7 @@ const repoCount=50;
 
 console.log(`Hi, my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String ('Hitesh');
+const gameName = new String ('Hitesh-hc-kuma');
 console.log(gameName[0]);
 console.log(gameName.__proto__);
 
@@ -16,5 +16,25 @@ console.log(gameName.charAt(2));
 
 console.log(gameName.indexOf('t'));
 
+const newString=gameName.substring(0,4);
+console.log(newString);
 
+const anotherString =gameName.slice(0,4);
+console.log(newString);
+
+// const anotherString =gameName.slice(-8,4);
+// console.log(anotherString);
+// trim
+const newStringone="  payal";
+console.log(newStringone);
+console.log(newStringone.trim());
+
+//replace
+const url="https://hitesh.com/hi%20kumar";
+url.replace('%20','_');
+
+console.log(url.replace('%20','_'));
+console.log(url.includes('hitesh'));
+
+console.log(gameName.split('-'));
 
