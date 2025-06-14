b01d0103330fcffa3c2ac45313cebe6480245e1c
payal12-PM <payalku0305@gmail.com>
2025-06-14 07:59:04 +0000

    lastobjwithjson

--- 01_basic/lastinobj.js
+++ 01_basic/lastinobj.js
@@ -1,15 +1,23 @@
 const course = {
     coursename: "js in english",
     price: "12345",
-    courseInstructor: "payal"  // use : not =
+    courseInstructor: "payal"  // Use colon : instead of equals =
 };
 
-// Object Destructuring
-const { courseInstructor: instructor } = course;
-console.log(instructor);
+// const { courseInstructor } = course;
 // console.log(courseInstructor);  // Output: payal
 
 
+
+
+
+// // Object Destructuring
+const { courseInstructor: instructor } = course;
+console.log(instructor);
+// // console.log(courseInstructor);  // Output: payal
+
+
+// API(rander userme api) xml and json:tools json formator
 
 
 
